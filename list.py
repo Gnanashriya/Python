86f6806f0c9a741f11540d620f28e3b88277d132
my-repo <maanasamadari730@gmail.com>
2026-04-04 21:33:59 +0530

    initial commit

--- list.py
+++
@@ -1,3 +0,0 @@
-numbers=[1,2,3,4,5]
-numbers.insert(6)
-print(numbers)
--- second.py
+++
@@ -1,5 +0,0 @@
-temperature=25
-if temperature<30:
-    print("It's a cool day")
-elif temperature>20:
-     print("It's a cool day")    
