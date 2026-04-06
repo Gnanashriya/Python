85de4f0f2bdaa671cac46aff0660bf87b4e0e349
Gnanashriya <shriyamasula@gmail.com>
2026-04-06 22:51:08 +0530

    syntax

---
+++ Day3/statements.py
@@ -0,0 +1,5 @@
+#statements (Semicolon-separated)
+print("hello "); print("world")
+#print without a new line
+print("Hello World!", end=" ")#here we add space for better readability
+print("Welcome to python programming language")
