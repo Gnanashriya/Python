dc5e32d72b155ee1cfaa23bea04d72662e2b599e
Gnanashriya <shriyamasula@gmail.com>
2026-04-07 23:20:45 +0530

    Basics

---
+++ Day4/Basics.py
@@ -0,0 +1,20 @@
+#print numders
+print(3)
+print(13)
+print(24)
+# maths in side print()
+print(3+4)
+print(5*2)
+
+#Mix text and numbers
+print("The sum of ",3+4,"is 7")
+print("The product of ",5*2,"is 10")
+
+#creating comments
+#single line comment
+print("This is a single line comment")
+
+#multi line comment
+"""This is 
+a multi line comment"""
+print("This is a multi line comment")
