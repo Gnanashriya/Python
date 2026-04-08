db1f77a681db458e7a4daf524ffc8ea098dc414b
Gnanashriya <shriyamasula@gmail.com>
2026-04-08 23:06:38 +0530

    variables

---
+++ Day5/variables.py
@@ -0,0 +1,18 @@
+#Creating Variables
+x=5
+y="Python"
+print(x)
+print(y)
+
+#variables do not declare any particular type
+i=15  #x is type of interger
+i='Hello'  #x is type of string
+print(i)
+
+#casting
+a = str(3)    # a will be '3'
+b = int(3)    # b will be 3
+c = float(3)  # c will be 3.0
+print(a)
+print(b)
+print(c)
