2332eaf062d6b4e8eb8cbd2665d9e02bd1e1613d
Gnanashriya <shriyamasula@gmail.com>
2026-04-09 22:55:16 +0530

    case-sensitive

---
+++ Day6/Type.py
@@ -0,0 +1,17 @@
+#Get the type of variable
+x=21
+y="Python"
+print(type(x))
+print(type(y))
+
+#single (or) double quotes
+a='Hello'
+print(a)
+b="Hello"
+print(b)
+
+#case sensitive
+i=15
+print(i)
+I='I will not overwrite i'
+print(I)
