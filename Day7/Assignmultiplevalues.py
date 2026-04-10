75c01c39d3ad87165279b888c2e33f4a6ea40700
Gnanashriya <shriyamasula@gmail.com>
2026-04-10 23:01:46 +0530

    Assignmultiplevalues

---
+++ Day7/Assignmultiplevalues.py
@@ -0,0 +1,37 @@
+#day7
+#legal variable names
+myvar="Shriya"
+Myvar="Shriya"
+myVar="Shriya"
+my_var="Shriya"
+_my_var="Shriya"
+myvar2="Shriya"
+MYVAR="SHRIYA"
+print(_my_var)
+print(MYVAR)
+
+#Multi words variable names
+#Camel Case
+myVariableName="Python"
+print(myVariableName)
+
+#Pascal Case
+MyVariableName="Hello"
+print(MyVariableName)
+
+#Snake case
+my_variable_name="World!"
+print(my_variable_name)
+
+
+#many values to multiple variables
+x,y,z="orange","banana","apple"
+print(x)
+print(y)
+print(z)
+
+#one value to multiple variables
+i=j=k="Cherry"
+print(i)
+print(j)
+print(k)
