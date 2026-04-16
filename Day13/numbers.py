5114790c6345a0f946b59b60f73345178b79e2d0
Gnanashriya <shriyamasula@gmail.com>
2026-04-16 23:05:04 +0530

    type conversion

---
+++ Day13/numbers.py
@@ -0,0 +1,61 @@
+#Python Numbers
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+print(type(x))
+print(type(y))
+print(type(z))
+
+#int
+a = 1
+b = 35656222554887711
+c = -3255522
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+#Float
+d = 1.10
+
+print(type(d))
+
+#"e" to indicate the power of 10
+e = 35e3
+f = 12E4
+g = -87.7e100
+
+print(type(e))
+print(type(f))
+print(type(g))
+
+#Complex
+h = 3+5j
+i = 5j
+j = -5j
+
+print(type(h))
+print(type(i))
+print(type(j))
+
+#Type conversion
+k = 1    # int
+l = 2.8  # float
+m = 1j   # complex
+
+#convert from int to float:
+m = float(k)
+
+#convert from float to int:
+n = int(l)
+
+#convert from int to complex:
+o = complex(m)
+
+print(m)
+print(n)
+print(o)
+
+print(type(m))
+print(type(n))
+print(type(o))
