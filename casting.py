61038c9dba092c1401b5e81fcfc86c344166b90f
Gnanashriya <shriyamasula@gmail.com>
2026-04-18 00:04:19 +0530

    casting

---
+++ casting.py
@@ -0,0 +1,34 @@
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+print(x)
+print(y)
+print(z)
+
+#integers:
+a = float(1)
+b = float(2.8)
+c = float("3")
+d = float("4.2")
+print(a)
+print(b)
+print(c)
+print(d)
+
+#float
+e = float(1)
+f = float(2.8)
+g = float("3")
+h = float("4.2")
+print(e)
+print(f)
+print(g)
+print(h)
+
+#strings
+i = str("s1")
+j = str(2)
+k = str(3.0)
+print(i)
+print(j)
+print(k)
