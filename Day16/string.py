9c48a69c9a4787f03a2989a1f26bdbf049cda3e6
Gnanashriya <shriyamasula@gmail.com>
2026-04-19 23:32:27 +0530

    string

---
+++ Day16/string.py
@@ -0,0 +1,17 @@
+#getting character 
+a = "Hello, World!"
+print(a[1])
+
+#String Length
+text = "Hello World"
+print(len(text))
+b = "Hello, World!"
+print(len(b))
+
+#Check String
+txt = "The best things in life are free!"
+print("free" in txt)
+#using if statement
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
