0e4313b666d6e91126b541c2ce05b3b4b5f46d22
Gnanashriya <shriyamasula@gmail.com>
2026-04-20 22:56:35 +0530

    slicing

---
+++ Day17/slicing.py
@@ -0,0 +1,20 @@
+#Check if NOT
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+
+#if statement
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
+else:
+  print("Yes,'expensive' is present.")
+
+#Slicing can return the range of string
+b = "Hello, World!"
+print(b[2:5])
+#Slice From the Start
+a='hello world'
+print(a[:5])
+#Slicing from end
+c="python is easy to learn"
+print(c[7:])
