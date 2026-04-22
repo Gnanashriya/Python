c8f003480732d3845a6434f47708da8abea8f5f4
Gnanashriya <shriyamasula@gmail.com>
2026-04-22 23:08:13 +0530

    concatenation

---
+++ Day19/string_concatenation.py
@@ -0,0 +1,22 @@
+a='Hello'
+b='World!'
+c=a+b
+print(c)
+
+d=a+' '+b
+print(d)
+
+#f-stings
+age=36
+txt=f'I am John, My age is {age}'
+print(txt)
+#place holder
+price=93
+info=f'the price is {price} dollar'
+print(info)
+#modifier
+info1=f'the price is {price:.1f} dollar'
+print(info1)
+#math operations
+math=f'the 12*3 is {12*3}'
+print(math)
