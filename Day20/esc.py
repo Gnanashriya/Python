3b376bd978fa94fdc48de01727ba81361df6cc44
Gnanashriya <shriyamasula@gmail.com>
2026-04-23 22:55:35 +0530

    concatenation

---
+++ Day20/esc.py
@@ -0,0 +1,21 @@
+txt="heyy hello \"i am\" a student"
+print(txt)
+#\'
+a='heyy hello \'i am\' a student'
+#\\
+b='heyy hello \\i am\\ a student'
+#\n=new line
+c='hello \nworld'
+#\r=carrage returns
+d='hello \rworld'
+#\t
+e='hello \tworld'
+#\b
+f='hello \bworld'
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+
