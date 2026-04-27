9c9d7fa0d9cda9bb7af3e513d5e271c1c0b185b2
Gnanashriya <shriyamasula@gmail.com>
2026-04-27 23:16:00 +0530

    operators

---
+++ Day24/operators.py
@@ -0,0 +1,25 @@
+#Arithmetic Operators
+x = 15
+y = 4
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
+
+#Division in Python
+x = 12
+y = 5
+
+print(x / y)
+x = 12
+y = 5
+
+print(x // y)
+numbers = [1, 2, 3, 4, 5]
+
+if (count := len(numbers)) > 3:
+    print(f"List has {count} elements")
