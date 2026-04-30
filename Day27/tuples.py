6db7704a70ec7847b16838a9c8ab6dcf99c2c039
Gnanashriya <shriyamasula@gmail.com>
2026-04-30 21:06:34 +0530

    list

---
+++ Day27/tuples.py
@@ -0,0 +1,10 @@
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+thist = ("apple", "banana", "cherry", "apple", "cherry")
+print(thist)
+thistu = ("apple",)
+print(type(thistu))
+
+#NOT a tuple
+this = ("apple")
+print(type(this))
