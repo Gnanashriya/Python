e3cfe89f6038b47ef47bfcb9c26871601a2fd684
Gnanashriya <shriyamasula@gmail.com>
2026-05-03 23:34:47 +0530

    tuples

---
+++ Day30/updatetuple.py
@@ -0,0 +1,13 @@
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:])
+
+#Range of Negative Indexes
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[-4:-1])
+
+#Check if Item Exists
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+  print("Yes, 'apple' is in the fruits tuple")
+
+
