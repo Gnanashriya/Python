6bdec26f83ef06c83e16efaed98acaf874a01b66
Gnanashriya <shriyamasula@gmail.com>
2026-05-04 22:46:20 +0530

    tuples

---
+++ Day31/t.py
@@ -0,0 +1,19 @@
+#Change Tuple Values(update)
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+
+print(x)
+
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.append("orange")
+thistuple = tuple(y)
+#Add tuple to a tuple
+thistuple = ("apple", "banana", "cherry")
+y = ("orange",)
+thistuple += y
+
+print(thistuple)
+
