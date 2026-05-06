5b91ad202e548d1fed31b1f408b8835444370087
Gnanashriya <shriyamasula@gmail.com>
2026-05-06 22:52:59 +0530

    tuples

---
+++ Day33/uptup.py
@@ -0,0 +1,17 @@
+#Using Asterisk*
+fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+
+(green, yellow, *red) = fruits
+
+print(green)
+print(yellow)
+print(red)
+
+#Add a list of values the "tropic" variable:
+fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+
+(green, *tropic, red) = fruits
+
+print(green)
+print(tropic)
+print(red)
